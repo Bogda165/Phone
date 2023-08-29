68f85c3f29705126de61a9a5d37fcfb650911533
Bogda165 <koval.b@icloud.com>
2023-08-29 16:58:43 +0200

    Initial Commit

---
+++ Phone/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Phone
+//
+//  Created by Богдан Коваль on 29.08.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
